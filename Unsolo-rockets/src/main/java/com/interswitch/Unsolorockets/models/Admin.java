6a470db837557b909b5d88cc6a14a28d2d9c2de6
Omisandefunmi <omisandefunm@gmail.com>
2023-08-31 09:34:18 +0100

    "added boiler plates"

---
+++ Unsolo-rockets/src/main/java/com/interswitch/Unsolorockets/models/Admin.java
@@ -0,0 +1,17 @@
+package com.interswitch.Unsolorockets.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "admins")
+public class Admin extends User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
