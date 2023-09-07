352f2c38f411410da4952065633687dd2b18a367
Omisandefunmi <omisandefunm@gmail.com>
2023-09-07 13:32:45 +0100

    "modified email verification"

--- Unsolo-rockets/src/main/java/com/interswitch/Unsolorockets/dtos/requests/OTPRequest.java
+++ Unsolo-rockets/src/main/java/com/interswitch/Unsolorockets/dtos/requests/OTPRequest.java
@@ -2,13 +2,11 @@ package com.interswitch.Unsolorockets.dtos.requests;
 
 
 import lombok.Data;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 @Data
 @RequiredArgsConstructor
 public class OTPRequest {
-//    @NonNull(message = "Enter 5-digit OTP sent to your phone")
-//    @Size(min = 5, max = 5)
-//    private String otp;
+
+    private String otp;
     private String emailForOTP;
 }
