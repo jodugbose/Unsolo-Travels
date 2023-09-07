65de2ce5716788cd9142321f69b49db424860eb3
Oluchukwu Okwuibe <Oluchukwu.Okwuibe@Interswitchng.com>
2023-09-07 07:55:05 +0100

    added surefire plugin to the pom.xl for test purpose.

--- Unsolo-rockets/src/main/java/com/interswitch/Unsolorockets/service/impl/EmailMailServiceImpl.java
+++ Unsolo-rockets/src/main/java/com/interswitch/Unsolorockets/service/impl/EmailMailServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.util.Date;
--- Unsolo-rockets/src/test/java/com/interswitch/Unsolorockets/UserServiceApplicationTests.java
+++ Unsolo-rockets/src/test/java/com/interswitch/Unsolorockets/UserServiceApplicationTests.java
@@ -1,13 +1,8 @@
 package com.interswitch.Unsolorockets;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UserServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
