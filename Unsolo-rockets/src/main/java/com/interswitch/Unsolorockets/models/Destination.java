cb919fdf33cd5e6ce0d30c6603bec5bd7d20ec7b
Omisandefunmi <omisandefunm@gmail.com>
2023-09-18 15:28:17 +0100

    added create package endpoint

--- Unsolo-rockets/src/main/java/com/interswitch/Unsolorockets/models/Destination.java
+++ Unsolo-rockets/src/main/java/com/interswitch/Unsolorockets/models/Destination.java
@@ -2,12 +2,14 @@ package com.interswitch.Unsolorockets.models;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
 @Entity
 @Table(name = "destinations")
+@NoArgsConstructor
 public class Destination {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
