f5f87aa1cb2547f518669f65921d3b93940e3452
Omisandefunmi <omisandefunm@gmail.com>
2023-09-19 12:02:07 +0100

    created deactivate account endpoint for admin

--- Unsolo-rockets/src/main/java/com/interswitch/Unsolorockets/controllers/AdminController.java
+++ Unsolo-rockets/src/main/java/com/interswitch/Unsolorockets/controllers/AdminController.java
@@ -2,14 +2,13 @@ package com.interswitch.Unsolorockets.controllers;
 
 import com.interswitch.Unsolorockets.dtos.requests.PackageDto;
 import com.interswitch.Unsolorockets.exceptions.PackageException;
+import com.interswitch.Unsolorockets.exceptions.UserAlreadyExistException;
+import com.interswitch.Unsolorockets.exceptions.UserNotFoundException;
 import com.interswitch.Unsolorockets.service.AdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,5 +20,11 @@ public class AdminController {
     public ResponseEntity<?> createPackage(@RequestBody PackageDto packageDto) throws PackageException {
         var response = adminService.createPackage(packageDto);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/")
+    public ResponseEntity<?> deactivateAccount(@RequestParam String email) throws UserNotFoundException, UserAlreadyExistException {
+        var response = adminService.deactivateUserAccount(email);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
