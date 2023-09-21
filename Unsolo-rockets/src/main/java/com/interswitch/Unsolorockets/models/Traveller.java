19746b488e50973074ab642188a0ed3f7cfb5761
phoenixsylvia <sylviananev@gmail.com>
2023-09-21 08:40:58 +0100

    resolved merge conflict

--- Unsolo-rockets/src/main/java/com/interswitch/Unsolorockets/models/Traveller.java
+++ Unsolo-rockets/src/main/java/com/interswitch/Unsolorockets/models/Traveller.java
@@ -6,19 +6,12 @@ import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
 
 @Setter
 @Getter
 @Entity
 @Table(name = "travellers")
 public class Traveller extends User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<FlightBooking> flightBookings = new ArrayList<>();
